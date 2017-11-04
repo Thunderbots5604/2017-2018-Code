ea0a241ea74c98bd425335827e98e5119b7948d2
knightsean00 <knightsean00@gmail.com>
2017-11-03 18:03:07 -0700

    Floaty drive and new arm + auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import java.lang.Math;
+
+@TeleOp(name="autoTesting", group="Autonomous")
+public class Autonomous extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        DcMotor leftMotorFront = null;
+        DcMotor leftMotorBack = null;
+
+        DcMotor rightMotorFront = null;
+        DcMotor rightMotorBack = null;
+
+        DcMotor arm = null;
+
+        DcMotor push = null;
+
+        leftMotorFront = hardwareMap.dcMotor.get("left_drive_front");
+        leftMotorBack = hardwareMap.dcMotor.get("left_drive_back");
+
+        rightMotorFront = hardwareMap.dcMotor.get("right_drive_front");
+        rightMotorBack = hardwareMap.dcMotor.get("right_drive_back");
+
+        arm = hardwareMap.dcMotor.get("arm");
+
+        push = hardwareMap.dcMotor.get("push");
+
+        rightMotorFront.setDirection(DcMotor.Direction.REVERSE);
+        rightMotorBack.setDirection(DcMotor.Direction.REVERSE);
+        leftMotorFront.setDirection(DcMotor.Direction.REVERSE);
+        leftMotorBack.setDirection(DcMotor.Direction.REVERSE);
+
+/*        leftMotorFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotorBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotorFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotorBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
+
+        waitForStart();
+        runtime.reset();
+
+        rightMotorFront.setPower(1.0);
+        rightMotorBack.setPower(1.0);
+        leftMotorFront.setPower(-1.0);
+        leftMotorBack.setPower(-1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.0)) {
+            telemetry.addLine("Moving forward");
+            telemetry.update();
+        }
+        telemetry.addLine("Done ;)");
+        telemetry.update();
+        leftMotorFront.setPower(0);
+        leftMotorBack.setPower(0);
+        rightMotorFront.setPower(0);
+        rightMotorBack.setPower(0);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -11,7 +12,7 @@ import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import java.lang.Math;
 
-@TeleOp(name="Testing", group="TeleOp")
+@TeleOp(name="TeleOp", group="TeleOp")
 public class TeleOpTest extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
@@ -28,8 +29,8 @@ public class TeleOpTest extends LinearOpMode {
         DcMotor rightMotorBack = null;
 
         DcMotor arm = null;
-
         DcMotor push = null;
+        DcMotor armtwo = null;
 
         leftMotorFront = hardwareMap.dcMotor.get("left_drive_front");
         leftMotorBack = hardwareMap.dcMotor.get("left_drive_back");
@@ -39,12 +40,16 @@ public class TeleOpTest extends LinearOpMode {
 
         arm = hardwareMap.dcMotor.get("arm");
 
+        armtwo = hardwareMap.dcMotor.get("armtwo");
+
         push = hardwareMap.dcMotor.get("push");
 
         rightMotorFront.setDirection(DcMotor.Direction.REVERSE);
         rightMotorBack.setDirection(DcMotor.Direction.REVERSE);
         leftMotorFront.setDirection(DcMotor.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotor.Direction.REVERSE);
+
+        armtwo.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         runtime.reset();
@@ -53,73 +58,82 @@ public class TeleOpTest extends LinearOpMode {
 
             //Stuff to display for Telemetry
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Left Front", "Ticks: " + leftMotorFront.getCurrentPosition());
+            telemetry.addData("Left Back", "Ticks: " + leftMotorBack.getCurrentPosition());
+            telemetry.addData("Right Front", "Ticks: " + rightMotorFront.getCurrentPosition());
+            telemetry.addData("Right Back", "Ticks: " + rightMotorBack.getCurrentPosition());
             telemetry.update();
 
             //Forward and backward moving method
-            if(gamepad1.left_stick_y != 0 && gamepad1.left_stick_x < .1 && gamepad1.left_stick_x > -.1) {
-                leftMotorFront.setPower(gamepad1.left_stick_y);
-                leftMotorBack.setPower(gamepad1.left_stick_y);
-
+            if(gamepad1.left_stick_y != 0 && gamepad1.left_stick_x == 0 && gamepad1.right_stick_x == 0) {
                 rightMotorFront.setPower(-gamepad1.left_stick_y);
                 rightMotorBack.setPower(-gamepad1.left_stick_y);
 
-                telemetry.addLine("Moving forward or Backward");
-                telemetry.update();
+                leftMotorFront.setPower(gamepad1.left_stick_y);
+                leftMotorBack.setPower(gamepad1.left_stick_y);
             }
-            //Turning methods in TeleOp
-            else if(gamepad1.right_stick_x > 0) {
-                leftMotorFront.setPower(-gamepad1.right_stick_x);
-                leftMotorBack.setPower(-gamepad1.right_stick_x);
+            else if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x > 0 && gamepad1.right_stick_x == 0) {
+                rightMotorFront.setPower(-gamepad1.left_stick_y - Math.abs(gamepad1.left_stick_x));
+                rightMotorBack.setPower(-gamepad1.left_stick_y);
 
-                rightMotorFront.setPower(-gamepad1.right_stick_x);
-                rightMotorBack.setPower(-gamepad1.right_stick_x);
+                leftMotorFront.setPower(gamepad1.left_stick_y);
+                leftMotorBack.setPower(gamepad1.left_stick_y + Math.abs(gamepad1.left_stick_x));
+            }
+            else if (gamepad1.left_stick_y == 0 && gamepad1.left_stick_x != 0 && gamepad1.right_stick_x == 0) {
+                leftMotorFront.setPower(-gamepad1.left_stick_x);
+                rightMotorFront.setPower(-gamepad1.left_stick_x);
+
+                leftMotorBack.setPower(gamepad1.left_stick_x);
+                rightMotorBack.setPower(gamepad1.left_stick_x);
+            }
+            else if(gamepad1.left_stick_y > 0 && gamepad1.left_stick_x > 0 && gamepad1.right_stick_x == 0) {
+                rightMotorFront.setPower(-gamepad1.left_stick_y);
+                rightMotorBack.setPower(-gamepad1.left_stick_y + Math.abs(gamepad1.left_stick_x));
+
+                leftMotorFront.setPower(gamepad1.left_stick_y - Math.abs(gamepad1.left_stick_x));
+                leftMotorBack.setPower(gamepad1.left_stick_y);
+            }
+            else if(gamepad1.left_stick_y > 0 && gamepad1.left_stick_x < 0 && gamepad1.right_stick_x == 0) {
+                rightMotorFront.setPower(-gamepad1.left_stick_y + Math.abs(gamepad1.left_stick_x));
+                rightMotorBack.setPower(-gamepad1.left_stick_y);
+
+                leftMotorFront.setPower(gamepad1.left_stick_y);
+                leftMotorBack.setPower(gamepad1.left_stick_y - Math.abs(gamepad1.left_stick_x));
+            }
+            else if(gamepad1.left_stick_y < 0 && gamepad1.left_stick_x < 0 && gamepad1.right_stick_x == 0) {
+                rightMotorFront.setPower(-gamepad1.left_stick_y);
+                rightMotorBack.setPower(-gamepad1.left_stick_y - Math.abs(gamepad1.left_stick_x));
+
+                leftMotorFront.setPower(gamepad1.left_stick_y + Math.abs(gamepad1.left_stick_x));
+                leftMotorBack.setPower(gamepad1.left_stick_y);
+            }
+            else if (gamepad1.left_stick_y !=0  && gamepad1.right_stick_x > 0) {
+                leftMotorFront.setPower(gamepad1.left_stick_y);
+                leftMotorBack.setPower(gamepad1.left_stick_y);
+
+                rightMotorFront.setPower(0);
+                rightMotorBack.setPower(0);
+            }
+            else if (gamepad1.left_stick_y != 0 && gamepad1.right_stick_x < 0) {
+                leftMotorFront.setPower(0);
+                leftMotorBack.setPower(0);
+
+                rightMotorFront.setPower(-gamepad1.left_stick_y);
+                rightMotorBack.setPower(-gamepad1.left_stick_y);
+            }
+            else if(gamepad1.right_stick_x > 0) {
+                leftMotorFront.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
+                leftMotorBack.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
+
+                rightMotorFront.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
+                rightMotorBack.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
             }
             else if(gamepad1.right_stick_x < 0) {
-                leftMotorFront.setPower(-gamepad1.right_stick_x);
-                leftMotorBack.setPower(-gamepad1.right_stick_x);
+                leftMotorFront.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
+                leftMotorBack.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
 
-                rightMotorFront.setPower(-gamepad1.right_stick_x);
-                rightMotorBack.setPower(-gamepad1.right_stick_x);
-            }
-
-            //Strafe Method
-            else if (gamepad1.left_stick_x != 0 && gamepad1.left_stick_y < .1 && gamepad1.left_stick_y > -.1) {
-                rightMotorBack.setPower(gamepad1.left_stick_x);
-                rightMotorFront.setPower(-gamepad1.left_stick_x);
-
-                leftMotorFront.setPower(-gamepad1.left_stick_x);
-                leftMotorBack.setPower(gamepad1.left_stick_x);
-            }
-
-            //Diagonal Strafe
-            else if(gamepad1.left_stick_x > .1 && gamepad1.left_stick_y < -.1) {
-                leftMotorFront.setPower(-gamepad1.left_stick_x);
-                rightMotorBack.setPower(gamepad1.left_stick_x);
-
-                leftMotorBack.setPower(0);
-                rightMotorFront.setPower(0);
-            }
-            else if(gamepad1.left_stick_x < -.1 && gamepad1.left_stick_y < -.1) {
-                leftMotorFront.setPower(0);
-                rightMotorBack.setPower(0);
-
-                leftMotorBack.setPower(gamepad1.left_stick_x);
-                rightMotorFront.setPower(-gamepad1.left_stick_x);
-            }
-
-            else if(gamepad1.left_stick_x > .1 && gamepad1.left_stick_y > .1) {
-                leftMotorFront.setPower(0);
-                rightMotorBack.setPower(0);
-
-                leftMotorBack.setPower(gamepad1.left_stick_x);
-                rightMotorFront.setPower(-gamepad1.left_stick_x);
-            }
-            else if(gamepad1.left_stick_x < -.1 && gamepad1.left_stick_y > .1) {
-                leftMotorFront.setPower(-gamepad1.left_stick_x);
-                rightMotorBack.setPower(gamepad1.left_stick_x);
-
-                leftMotorBack.setPower(0);
-                rightMotorFront.setPower(0);
+                rightMotorFront.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
+                rightMotorBack.setPower(-gamepad1.right_stick_x * Math.abs(gamepad1.right_stick_x) * Math.abs(gamepad1.right_stick_x));
             }
             else {
                 leftMotorFront.setPower(0);
@@ -129,20 +143,23 @@ public class TeleOpTest extends LinearOpMode {
             }
 
             if(gamepad1.dpad_up) {
-                arm.setPower(.25);
+                arm.setPower(-1);
+                armtwo.setPower(-1);
             }
             else if (gamepad1.dpad_down) {
-                arm.setPower(-.25);
+                arm.setPower(1);
+                armtwo.setPower(1);
             }
             else {
                 arm.setPower(0);
+                armtwo.setPower(0);
             }
 
-            if(gamepad1.a || gamepad2.a) {
-                push.setPower(1);
+            if(gamepad2.a) {
+                push.setPower(.5);
             }
-            else if (gamepad1.b || gamepad2.b) {
-                push.setPower(-1);
+            else if (gamepad2.b) {
+                push.setPower(-.5);
             }
             else {
                 push.setPower(0);
